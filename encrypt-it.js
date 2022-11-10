815c9922c10f035c4714ae6b35021571b55a3971
Val2049 <97463600+Val2049@users.noreply.github.com>
2022-11-09 20:57:48 -0800

    Changes to a5

--- encrypt-it.js
+++ encrypt-it.js
@@ -6,28 +6,16 @@ console.log("Window loaded!");
 (function() {
   "use strict";
 
-  /**
-   * The starting point in our program, setting up a listener
-   * for the "load" event on the window, signalling the HTML DOM has been constructed
-   * on the page. When this event occurs, the attached function (init) will be called.
-   */
   window.addEventListener("load", init);
 
-  /**
-   * TODO: Write a function comment using JSDoc.
-   */
   function init() {
-    // Note: In this function, we usually want to set up our event handlers
-    // for UI elements on the page.
     let encryptButton = document.getElementById("encrypt-it");
     encryptButton.addEventListener("click", handleClick);
     let resetButton = document.getElementById("reset");
     resetButton.addEventListener("click", reset)
   }
 
-  // Add any other functions in this area (you should not implement your
-  // entire program in the init function, for similar reasons that
-  // you shouldn't write an entire Java program in the main method).
+
   function handleClick() {
     console.log("Window loaded!");
     let inputText = document.getElementById("input-text");
